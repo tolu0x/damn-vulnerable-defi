dbe339f771a383d717b6ab0a83c2c8bc38cb6baf
tolu0x <kalejaiyetoluwalase204@gmail.com>
2023-11-04 11:51:44 +0000

    complete unstoppable challenge

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -1,58 +1,71 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Unstoppable', function () {
-    let deployer, player, someUser;
-    let token, vault, receiverContract;
+describe("[Challenge] Unstoppable", function () {
+  let deployer, player, someUser;
+  let token, vault, receiverContract;
 
-    const TOKENS_IN_VAULT = 1000000n * 10n ** 18n;
-    const INITIAL_PLAYER_TOKEN_BALANCE = 10n * 10n ** 18n;
+  const TOKENS_IN_VAULT = 1000000n * 10n ** 18n;
+  const INITIAL_PLAYER_TOKEN_BALANCE = 10n * 10n ** 18n;
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
 
-        [deployer, player, someUser] = await ethers.getSigners();
+    [deployer, player, someUser] = await ethers.getSigners();
 
-        token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
-        vault = await (await ethers.getContractFactory('UnstoppableVault', deployer)).deploy(
-            token.address,
-            deployer.address, // owner
-            deployer.address // fee recipient
-        );
-        expect(await vault.asset()).to.eq(token.address);
+    token = await (
+      await ethers.getContractFactory("DamnValuableToken", deployer)
+    ).deploy();
+    vault = await (
+      await ethers.getContractFactory("UnstoppableVault", deployer)
+    ).deploy(
+      token.address,
+      deployer.address, // owner
+      deployer.address // fee recipient
+    );
+    expect(await vault.asset()).to.eq(token.address);
 
-        await token.approve(vault.address, TOKENS_IN_VAULT);
-        await vault.deposit(TOKENS_IN_VAULT, deployer.address);
+    await token.approve(vault.address, TOKENS_IN_VAULT);
+    await vault.deposit(TOKENS_IN_VAULT, deployer.address);
 
-        expect(await token.balanceOf(vault.address)).to.eq(TOKENS_IN_VAULT);
-        expect(await vault.totalAssets()).to.eq(TOKENS_IN_VAULT);
-        expect(await vault.totalSupply()).to.eq(TOKENS_IN_VAULT);
-        expect(await vault.maxFlashLoan(token.address)).to.eq(TOKENS_IN_VAULT);
-        expect(await vault.flashFee(token.address, TOKENS_IN_VAULT - 1n)).to.eq(0);
-        expect(
-            await vault.flashFee(token.address, TOKENS_IN_VAULT)
-        ).to.eq(50000n * 10n ** 18n);
+    expect(await token.balanceOf(vault.address)).to.eq(TOKENS_IN_VAULT);
+    expect(await vault.totalAssets()).to.eq(TOKENS_IN_VAULT);
+    expect(await vault.totalSupply()).to.eq(TOKENS_IN_VAULT);
+    expect(await vault.maxFlashLoan(token.address)).to.eq(TOKENS_IN_VAULT);
+    expect(await vault.flashFee(token.address, TOKENS_IN_VAULT - 1n)).to.eq(0);
+    expect(await vault.flashFee(token.address, TOKENS_IN_VAULT)).to.eq(
+      50000n * 10n ** 18n
+    );
 
-        await token.transfer(player.address, INITIAL_PLAYER_TOKEN_BALANCE);
-        expect(await token.balanceOf(player.address)).to.eq(INITIAL_PLAYER_TOKEN_BALANCE);
+    await token.transfer(player.address, INITIAL_PLAYER_TOKEN_BALANCE);
+    expect(await token.balanceOf(player.address)).to.eq(
+      INITIAL_PLAYER_TOKEN_BALANCE
+    );
 
-        // Show it's possible for someUser to take out a flash loan
-        receiverContract = await (await ethers.getContractFactory('ReceiverUnstoppable', someUser)).deploy(
-            vault.address
-        );
-        await receiverContract.executeFlashLoan(100n * 10n ** 18n);
-    });
+    // Show it's possible for someUser to take out a flash loan
+    receiverContract = await (
+      await ethers.getContractFactory("ReceiverUnstoppable", someUser)
+    ).deploy(vault.address);
+    await receiverContract.executeFlashLoan(100n * 10n ** 18n);
+  });
 
-    it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+  it("Execution", async function () {
+    /** CODE YOUR SOLUTION HERE */
+    /*
+            The goal of this challenge is to cause a denial of service to the flashLoan function of the UnstoppableVault contract. We can achieve that by making one of the conditional statements revert, thereby making it unusable.
+            There is a conditional statement that checks if the amount of shares of the totalSupply is equal to the totalAssets in the contract and it reverts if it's false. We can make this line revert by changing the state of the contract's balance of DVT. This can be achieved by making a direct ERC20 transfer of any amount of DVT to the vault.
+            Here is the code below.
+        */
 
-    after(async function () {
-        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+    await token.connect(player).transfer(vault.address, 2);
+  });
 
-        // It is no longer possible to execute flash loans
-        await expect(
-            receiverContract.executeFlashLoan(100n * 10n ** 18n)
-        ).to.be.reverted;
-    });
+  after(async function () {
+    /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+
+    // It is no longer possible to execute flash loans
+    await expect(
+      receiverContract.executeFlashLoan(100n * 10n ** 18n)
+    ).to.be.reverted;
+  });
 });
